af26f4b0573b97a0018837724f8b2fe45c39975d
mathlimma <mal3@cin.ufpe.br>
2021-02-11 22:26:37 -0300

    challenge 2

--- src/com/mefius/interBootcamp/ConjuntosBonsOuRuins.java
+++ src/com/mefius/interBootcamp/ConjuntosBonsOuRuins.java
@@ -1,23 +1,43 @@
 package com.mefius.interBootcamp;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ConjuntosBonsOuRuins {
 
     public static void main (String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        Scanner sc = new Scanner(System.in, StandardCharsets.ISO_8859_1);
+        List<String> set = new ArrayList<String>();
 
-        Map<String,String> abreviations = new HashMap<String, String>();
+        while (sc.hasNextLine()) {
+            int numberOfLines = Integer.parseInt(sc.nextLine().trim());
 
-        while(st.hasMoreTokens()) {
-            String currentToken = st.nextToken().trim();
-            if (currentToken.equals("0")) break;
+            if (numberOfLines != 0) {
+                for (int i = 0; i < numberOfLines; i++) {
+                    String currentToken = sc.nextLine().trim();
+                    set.add(currentToken);
+                }
+                boolean isBad = false;
+                for (String word : set) {
+                    Stream<String> stringStream = set.stream().filter((el) -> el.contains(word));
+                    List<String> stList = stringStream.collect(Collectors.toList());
+
+                    if (stList.size()>1) isBad = true;
+                }
+                set.clear();
+                if (isBad) {
+                    System.out.println("Conjunto Ruim");
+                } else {
+                    System.out.println("Conjunto Bom");
+                }
+            }else{
+                break;
+            }
         }
     }
 }
